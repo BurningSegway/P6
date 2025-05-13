8b63e4643535b326eff9fb1b640f21754cb47c00
bordlampe123 <bebj2792@gmail.com>
2025-05-13 14:27:59 +0200

    done for idag med yolo

--- YOLO/YOLO kode og data eksempel/train_rocks.py
+++ YOLO/YOLO kode og data eksempel/train_rocks.py
@@ -1,7 +1,15 @@
+#from ultralytics import YOLO, checks, hub
+#checks()
+
+#hub.login('ultralytics_api_key_here')
+
+#model = YOLO('https://hub.ultralytics.com/models/drsVaD3IRc5Cmx2HQQeW')
+#results = model.train(device='mps')
+
 from ultralytics import YOLO, checks, hub
 checks()
 
-hub.login('ultralytics_api_key_here')
+hub.login('bb45ab1ce9f6990415bb1868e761b3bea39b8c0b01')
 
-model = YOLO('https://hub.ultralytics.com/models/drsVaD3IRc5Cmx2HQQeW')
+model = YOLO('https://hub.ultralytics.com/models/ogWRIrcFlewDC3HOCDf4')
 results = model.train(device='mps')
--- YOLO/YOLO kode og data eksempel/train_rocks_own_URL+api.py
+++
@@ -1,8 +0,0 @@
-#from ultralytics import YOLO, checks, hub
-#checks()
-
-#hub.login('ultralytics_api_key_here')
-
-#model = YOLO('https://hub.ultralytics.com/models/drsVaD3IRc5Cmx2HQQeW')
-#results = model.train(device='mps')
-
